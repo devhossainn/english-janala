56f15be7ec9c371f1eccf7e13bfa4dd5b5357010
Hossain Ahmed <hossainpgmr@gmail.com>
2025-09-07 09:39:38 +0600

    create navbar with responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
-const doBlack = () => {
-  document.body.style.background = "black";
-};
-const doWhite = () => {
-  document.body.style.background = "white";
-};
+// const doBlack = () => {
+//   document.body.style.background = "black";
+// };
+// const doWhite = () => {
+//   document.body.style.background = "white";
+// };
+console.log("English janala console successfully");
